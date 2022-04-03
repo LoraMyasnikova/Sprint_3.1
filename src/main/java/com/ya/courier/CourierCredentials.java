6e36e02ac37ed1d60fefe9f86cfdc4e5930577ac
Lora Myasnikova <loramya@yandex.ru>
2022-04-03 17:33:29 +0700

    Add Courier, CourierClient, CourierCredentials, ScooterRestClient

---
+++ src/main/java/com/ya/courier/CourierCredentials.java
@@ -0,0 +1,15 @@
+package com.ya.courier;
+
+import lombok.Data;
+
+@Data
+public class CourierCredentials {
+
+    private String login;
+    private String password;
+
+    public CourierCredentials(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+}
